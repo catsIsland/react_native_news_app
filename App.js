7878676520ea225ae720a17afd0bead8dc3a9431
cats_island <yaminaye98@gmail.com>
2023-08-06 16:55:34 +0900

    ダミデータでニュース一覧を表示

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import { ListItem } from './components/ListItem';
+import articles from './dummies/articles';
 
 export default function App() {
+  const items = articles.map((article, index) => {
+    return (
+      <ListItem
+        imageUrl={article.urlToImage}
+        title={article.title}
+        author={article.author}
+        key={index.toString()}
+      />
+    )
+  })
 
   return (
     <View style={styles.container}>
-      <ListItem
-        imageUrl={'https://picsum.photos/id/20/200/300'}
-        title={'Both Android and iOS allow you to display formatted text by annotating Both Android and iOS allow you to display formatted text by annotating'}
-        author={'from ANAB'}
-      />
+      {items}
       <StatusBar style="auto" />
     </View >
   );
@@ -18,7 +25,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1, // 画面を全部使うという意味
     backgroundColor: '#eee',
     alignItems: 'center',
     justifyContent: 'center',
