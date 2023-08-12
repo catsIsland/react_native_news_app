eefba630c68cc70f46afd11b014abeeb004ffe34
cats_island <yaminaye98@gmail.com>
2023-08-12 14:44:09 +0900

    keyをapp jsonに保存

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, SafeAreaView, FlatList } from 'react-native';
 import { ListItem } from './components/ListItem';
+import Constants from 'expo-constants';
 import axios from 'axios';
 
-const URL = 'https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=3d32cbfa053247169a04614588d08459'
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${Constants.manifest.extra.newsApiKey}`
 
 export default function App() {
   const [articles, setArticles] = useState([]);
@@ -12,6 +13,7 @@ export default function App() {
   const fetchArticles = async () => {
     try {
       const response = await axios.get(URL);
+      console.log(response.data.articles);
       setArticles(response.data.articles);
     } catch (error) {
       console.error(error)
