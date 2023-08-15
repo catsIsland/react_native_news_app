f3f8fb0b5c87f667a7f05539d520798b8186e261
cats_island <yaminaye98@gmail.com>
2023-08-15 14:09:09 +0900

    ページ遷移して詳細をwebviewで表示

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -34,7 +34,7 @@ export const HomeScreen = ({ navigation }) => {
               imageUrl={item.urlToImage}
               title={item.title}
               author={item.author}
-              onPress={() => navigation.navigate('Article')}
+              onPress={() => navigation.navigate('Article', { article: item })}
             />
           )
         }
